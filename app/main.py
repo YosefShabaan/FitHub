02875124fa116c6a95e5be27852048aa0ffe2a32
Yosef Ahmed Shabaan <yosefshabaan05@gmail.com>
2026-05-16 02:41:32 +0300

    Refactor FastAPI routes and imports

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI
-
 from app.db.database import engine, Base
-
 from app.models.user import User
-
-from app.api.routes import auth
+from app.models.member import Member
+from app.models.subscription import Subscription
+from app.api.routes import auth, members, subscriptions, dashboard
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FitHub API")
 
-app.include_router(
-    auth.router,
-    prefix="/auth",
-    tags=["Authentication"]
-)
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(members.router, prefix="/members", tags=["Members"])
+app.include_router(subscriptions.router, prefix="/subscriptions", tags=["Subscriptions"])
+app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 
 @app.get("/")
 def root():
-    return {
-        "message": "FitHub API is running 🚀"
-    }
+    return {"message": "FitHub API is running 🚀"}
