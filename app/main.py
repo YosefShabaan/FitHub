685b4d9f6342b78ca4f42848ee10e64168ee965e
YosefShabaan <Yosefshabaan05@gmail.com>
2026-05-15 18:15:28 +0300

    setup database and user model

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from app.db.database import engine, Base
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="FitHub API")
 
 @app.get("/")
