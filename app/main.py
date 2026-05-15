462a4a79d233fa87f8bfaa2fb25b16739bf09018
YosefShabaan <Yosefshabaan05@gmail.com>
2026-05-15 18:33:14 +0300

    implement JWT authentication system

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 
 from app.db.database import engine, Base
+
 from app.models.user import User
+
+from app.api.routes import auth
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FitHub API")
+
+app.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["Authentication"]
+)
 
 @app.get("/")
 def root():
